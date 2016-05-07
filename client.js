0645441df412a5089d82b403ad6c8cbf61153a85
claire <cdersky@gmail.com>
2016-05-07 10:46:48 -0700

    initial commit

---
+++ client.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const fs = require('fs');
+const hostname = 'localhost';
+const port = 8080;
+const url = 'http://127.0.0.1:8080';
+
+
+var method = process.argv[2] || 'GET';
+
+if (method.toUpperCase() === 'GET'){
+  // //////////////// GET REQUEST
+  http.get(url, res => {
+    console.log('written in the CLIENT file: Got response: ', res.statusCode, res.statusMessage);
+
+    res.on('data', chunk => {
+      console.log('chunk:', chunk.toString());
+
+    
+    }).on('close', function(){
+      console.log('written in the CLIENT file: CLOSE event heard');
+
+    }).on('end', function(){
+      console.log('written in the CLIENT file: END event heard');
+
+    }).on('error', e => {
+      console.log('Got error: ', e.message);
+    });
+  });
+} else {
+
+//////////////// POST REQUEST
+  var options = {
+    hostname: hostname,
+    port: port,
+    method: 'POST'
+  };
+
+  var req = http.request(options, res => {
+    console.log('logging from http.request (in client)');
+
+    // res.on('end', () => {
+    //   console.log('No more data in response.');
+    // console.log('END');
+    // });
+  });
+
+  req.on('error', e => {
+    console.log(`problem with request: ${e.message}`);
+  });
+
+  // open a readStream
+  var rs = fs.createReadStream('clientToServer.txt', 'utf8', function(err, data){
+    // if(err){ // check for error
+    //   console.log('Error reading in file in the client file', err);
+    // } else {
+    //   console.log('DATA reading in file in the client file', data);
+    // }
+    // req.end(); // end
+  });
+
+  // pipe the read stream to the desired destination
+  rs.pipe(req);
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+
+
+// create a server
+const server = http.createServer((req, res) => {
+
+  // data recevied from the client
+  // can this be replace with a stream?
+  req.on('data', chunk => {
+    console.log('chunk from client:', chunk.toString());
+  });  
+
+  // data received from a file
+  // can this be replace with a stream?
+  res.on('data', chunk => {
+    console.log('chunk from file:', chunk.toString());
+  });
+
+  // send info from sendMe.txt to the client with fs
+  fs.readFile('sendMe.txt', 'utf8', (err, data) => {
+    if(err) { // check for error
+      console.log('error reading in file in the server file', err);
+    
+    } else { // if no error, send data
+      console.log('sending data to client');
+      res.write(data); // send data
+      
+      res.emit('close'); // force close event
+
+      res.end('Goodbye from the server file.'); // end 
+      
+    }
+  });
+  
+
+});
+
+const port = 8080;
+console.log('listening on port ', port);
+server.listen(port); // open server
